109eda537e3dc4dde60e6427df1a324f0ff898be
chaplin89 <2357260+chaplin89@users.noreply.github.com>
2024-06-05 09:09:45 +0200

    Fixed updater; Added action to publish on test pypi

--- helix/utility/updater.py
+++ helix/utility/updater.py
@@ -1,25 +1,65 @@
 import subprocess
 import sys
-from importlib.metadata import version
+from xml.etree import ElementTree
+
+import requests
+from packaging.version import Version
+
+from helix import VERSION
 
 
 class Updater:
 
     def __init__(self) -> None:
-        self.packages = ["Helix", "doublehelix-external"]
-        # Package Name -> (Current v., Latest v.)
-        self.versions = {x: (version(x), self.check(x)) for x in self.packages}
+        self.packages = ["doublehelix", "doublehelix-external"]
 
-    def check(self, package_name):
-        latest_version = subprocess.check_output(
-            [sys.executable, "-m", "pip", "index", "versions", package_name]
-        ).decode("utf-8")
-        latest_version = latest_version.split("Version: ")[1].split("\n")[0]
-        return latest_version
+    def check(self, stable_only=False):
+        r = requests.get("https://pypi.org/rss/project/doublehelix/releases.xml")
+        r.raise_for_status()
+        rss_feed = ElementTree.fromstring(r.text)
+        channel = rss_feed.find("channel")
+        versions = []
+        for item in channel.findall("item"):
+            title = item.find("title").text
+            version = Version(title)
+            versions.append(version)
+
+        if stable_only:
+            filtered_versions = []
+            for version in versions:
+                v = Version(version)
+                if v.is_devrelease or v.is_postrelease or v.is_prerelease:
+                    continue
+                filtered_versions.append(version)
+            versions = filtered_versions
+
+        if len(versions) == 0:
+            return None
+
+        versions.sort()
+
+        latest = versions[-1]
+        current = Version(VERSION.version)
+
+        # In case you're using a dev version of a public release, that's the most
+        # recent one.
+        if latest.pre == current.pre and latest.base_version == current.base_version:
+            return None
+
+        if latest > current:
+            return latest
+        return None
 
     def update(self, package_name, versions):
         subprocess.check_call(
-            [sys.executable, "-m", "pip", "install", "--upgrade", package_name]
+            [
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "--upgrade",
+                f"{package_name}=={versions}",
+            ]
         )
         print(f"Upgraded {package_name} from {versions[0]} to {versions[1]}")
 
@@ -27,5 +67,7 @@ class Updater:
 
 
 if __name__ == "__main__":
-    a = Updater()
-    pass
+    updater = Updater()
+    latest = updater.check()
+    if latest is not None:
+        updater.update(latest)
