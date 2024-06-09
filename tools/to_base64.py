d096620bca081d3c205057a3bb406ff43652e678
chaplin89 <2357260+chaplin89@users.noreply.github.com>
2024-06-09 12:09:57 +0200

    to_base64 is now producing files in a proper python dict format

--- tools/to_base64.py
+++ tools/to_base64.py
@@ -1,15 +1,10 @@
 import base64
 from pathlib import Path
+import sys
 
-
-files = [
-    Path("test", "data", "fake_genome_bgzip.fasta.gz"),
-    Path("test", "data", "fake_genome.fasta.gz"),
-    Path("test", "data", "fake_genome.fasta"),
-    Path("test", "data", "fake_genome.zip"),
-]
-
-for file in files:
-    with file.open("rb") as f:
-        b = f.read()
-        print(f'{file.name}="{base64.standard_b64encode(b).decode("utf-8")}"')
+if len(sys.argv) < 2:
+    exit()
+path = Path(sys.argv[1])
+with path.open("rb") as f:
+    b = f.read()
+    print(f'"{path.name}" : "{base64.standard_b64encode(b).decode("utf-8")}"')
