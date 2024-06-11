cb44942db74a3803d0a17ee82d596e4ee76323d2
chaplin89 <2357260+chaplin89@users.noreply.github.com>
2024-06-11 23:23:41 +0200

    AlignmentMapFileInfoAdapter is now producing correct data

--- helix/adapters/alignment_map_file_info_adapter.py
+++ helix/adapters/alignment_map_file_info_adapter.py
@@ -1,34 +1,50 @@
+from collections import OrderedDict
 from helix.data.alignment_map.alignment_map_file_info import AlignmentMapFileInfo
 from helix.data.tabular_data import TabularData, TabularDataRow
+from helix.reference.reference import ReferenceStatus
 from helix.utility.unit_prefix import UnitPrefix
 
 
 class AlignmentMapFileInfoAdapter:
-    def adapt(stats: AlignmentMapFileInfo):
-        label_map = {
-            "path": "Path",
-            "sorted": "Sorted",
-            "indexed": "Indexed",
-            "file_type": "File type",
-            "reference_genome": "Reference genome",
-            "content": "Content",
-            "mitochondrial_dna_type": "Mitochondrial DNA type",
-            "name_type_chromosomes": "Name type chromosomes",
-            "name_type_mtdna": "Name type mtDNA",
-            "sequence_count": "Sequence count",
-            "gender": "Gender",
-        }
+    def adapt(stats: AlignmentMapFileInfo) -> TabularData:
+        label_map = OrderedDict(
+            [
+                ("sorted", "Sorted"),
+                ("indexed", "Indexed"),
+                ("file_type", "File type"),
+                ("content", "Content"),
+                ("gender", "Gender"),
+            ]
+        )
 
-        data = {x: None for x in label_map.values()}
-        data["Sorted"] = [stats.sorted.name]
-        data["Mitochondrial DNA type"] = [stats.mitochondrial_dna_model.name]
-        data["Name type mtDNA"] = [stats.name_type_mtdna.name]
-        data["Name type chromosomes"] = [stats.name_type_chromosomes.name]
-        data["Gender"] = [stats.gender.name]
-        data["File type"] = [stats.file_type.name]
-        data["Path"] = [str(stats.path)]
+        data = OrderedDict()
+        data["Directory"] = [str(stats.path.parent)]
+        data["Filename"] = [stats.path.name]
         data["Size"] = [UnitPrefix.convert_bytes(stats.path.stat().st_size)]
+        data["File type"] = [stats.file_type.name]
+        if stats.reference_genome.status == ReferenceStatus.Available:
+            data["Reference"] = [
+                f"Based on GRCh{stats.reference_genome.build}, reference is available"
+            ]
+        elif stats.reference_genome.status == ReferenceStatus.Buildable:
+            data["Reference"] = [
+                f"Likely based on GRCh{stats.reference_genome.build}, reference can be built"
+            ]
+        elif stats.reference_genome.status == ReferenceStatus.Downloadable:
+            data["Reference"] = [
+                f"Based on GRCh{stats.reference_genome.build}, reference can be downloaded"
+            ]
+        elif stats.reference_genome.status == ReferenceStatus.Unknown:
+            data["Reference"] = [
+                f"Likely based on GRCh{stats.reference_genome.build}. reference is unknown"
+            ]
+        data["Gender"] = [stats.gender.name]
+        data["Sorted"] = [stats.sorted.name]
+        data["Mitochondrial DNA Model"] = [stats.mitochondrial_dna_model.name]
+
         for key, value in label_map.items():
+            if value == "Path":
+                continue
             if value not in data or data[value] is None:
                 data[value] = [str(stats.__dict__[key])]
         data = TabularData(None, [TabularDataRow(x[0], x[1]) for x in data.items()])
