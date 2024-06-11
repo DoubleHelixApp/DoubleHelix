83e2e5c0e6d2038991f37482e9764fe62ea27964
chaplin89 <2357260+chaplin89@users.noreply.github.com>
2024-06-11 08:26:09 +0200

    Added doc for regions.py

--- helix/utility/regions.py
+++ helix/utility/regions.py
@@ -13,12 +13,36 @@ class RegionType(Flag):
 
 
 class Regions:
+    """Represent a region in a genome.
+
+    This class loads and manipulate .bed file templates containing regions in a genome.
+    Since the bed files need to change according to the specific sequence naming
+    of the loaded file, this class take care of loading the templates for these
+    .bed files and convert them to the correct sequence naming.
+    """
+
     def __init__(self, config=MANAGER_CFG.REPOSITORY, converter=Converter()) -> None:
         self._templates = config.metadata.joinpath("bed_templates")
         self._temporary = config.temporary
         self._converter = converter
 
     def get_path(self, build: str, type: RegionType, sequences: list[Sequence]):
+        """Return the path of a .bed file that is built for a specific genome.
+
+        Args:
+            build (str): The build number (currently only 37 or 38 are supported)
+            type (RegionType): RegionType of the region the caller is looking for.
+                Currently only Y or MT or WES are supported. Regions can be combined
+                but only Y | MT is a valid combination.
+            sequences (list[Sequence]): List of sequences from the input file.
+
+        Raises:
+            ValueError: An invalid combination of RegionTypes was provided.
+            FileNotFoundError: Template file for the provided build was not found.
+
+        Returns:
+            pathlib.Path: Path to the.bed file that is built for a specific genome.
+        """
         name_map = {x.canonic_name: x.name for x in sequences}
 
         suffix = ""
@@ -54,5 +78,14 @@ class Regions:
         return temporary_file
 
     def get_content(self, build: str, type: RegionType):
+        """Build and get the content of a .bed file.
+
+        Args:
+            build (str): Build name.
+            type (RegionType): RegionType of the region.
+
+        Returns:
+            str: Content of the .bed file.
+        """
         file = self.get_path(build, type)
         return file.read_text("utf-8")
