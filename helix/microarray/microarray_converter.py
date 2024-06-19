3aeaefd4ee842045e6aafaf4a892fb5c69c4b03f
chaplin89 <2357260+chaplin89@users.noreply.github.com>
2024-06-19 10:17:54 +0200

    MicroarrayConverter and RawFile now support a new template format that have a ## at the beginning with some metadata for the template

--- helix/microarray/microarray_converter.py
+++ helix/microarray/microarray_converter.py
@@ -1,10 +1,9 @@
+import gzip
 import logging
-from collections import OrderedDict
 from pathlib import Path
 
 from helix.configuration import MANAGER_CFG
 from helix.data.microarray_converter import MicroarrayConverterTarget
-from helix.microarray.microarray_line_formatter import TARGET_FORMATTER_MAP
 from helix.microarray.raw_file import RawEntry, RawFile
 from helix.naming.converter import Converter
 
@@ -20,23 +19,6 @@ class MicroarrayConverter:
                 f"Unable to find microarray template body folder at {self._template_folder!s}"
             )
 
-    def _group_entries(
-        self, path
-    ) -> OrderedDict[str, OrderedDict[int, list[RawEntry]]]:
-        grouped: OrderedDict[str, OrderedDict[int, list[RawEntry]]] = OrderedDict()
-        for entry in RawFile(path):
-            if entry is None:
-                continue
-            elif isinstance(entry, str):
-                continue
-            chromosome = Converter.canonicalize(entry.chromosome)
-            if chromosome not in grouped:
-                grouped[chromosome] = OrderedDict()
-            if entry.position not in grouped[chromosome]:
-                grouped[chromosome][entry.position] = []
-            grouped[chromosome][entry.position].append(entry)
-        return grouped
-
     def _null_result(self, target: MicroarrayConverterTarget):
         if target in [
             MicroarrayConverterTarget.Ancestry_v1,
@@ -45,86 +27,93 @@ class MicroarrayConverter:
             return "00"
         return "--"
 
-    def ingest(self, target: MicroarrayConverterTarget):
-        body_path = self._template_folder.joinpath(target.name).with_suffix(".txt.gz")
+    def ingest(self, input: Path):
+        name_noext = input.name.removesuffix("".join(input.suffixes))
+        target = self._template_folder.joinpath(name_noext).with_suffix(".txt.gz")
 
-        if not body_path.exists():
+        if target == input:
+            raise ValueError(f"Input file is the same as the target: {input!s}")
+
+        if not input.exists():
             raise FileNotFoundError(
-                f"Unable to find body file for microarray template at {body_path!s}"
+                f"Unable to find body file for microarray template at {input!s}"
             )
 
-        template = self._group_entries(body_path)
+        rawfile = RawFile(input)
+        template = rawfile.parse()
+
         lines = []
-        for chromosome in Converter.sort(template.keys()):
-            ordered_positions = list(template[chromosome].keys())
+        for chromosome in Converter.sort(template.grouped_entries.keys()):
+            ordered_positions = list(template.grouped_entries[chromosome].keys())
             ordered_positions.sort()
             for position in ordered_positions:
-                entries = set(x.template_entry for x in template[chromosome][position])
-                lines.extend(entries)
+                lines.extend(template.grouped_entries[chromosome][position])
 
-        with body_path.with_name(body_path.stem).open("wt") as file:
+        with gzip.open(target, "wt") as file:
+            if template.meta is not None:
+                file.write(f"##{template.meta.model_dump_json()}\n")
+            file.writelines(template.comments)
             file.writelines(lines)
 
-    def convert(self, input: Path, target: MicroarrayConverterTarget):
-        templates = self._template_folder.joinpath(target.name).with_suffix(".txt.gz")
-
-        if not templates.exists():
+    def convert(self, input: Path, target: Path):
+        if not target.exists():
             raise FileNotFoundError(
-                f"Unable to find body file for microarray template at {templates!s}"
+                f"Unable to find body file for microarray template at {target!s}"
             )
 
-        template = self._group_entries(templates)
-        query_entries = self._group_entries(input)
+        template = RawFile(target).parse()
+        query_entries = RawFile(input).parse()
 
         lines = []
-
-        for chromosome in template:
-            for position in template[chromosome]:
-                template_entries = template[chromosome][position]
+        for chromosome in template.grouped_entries:
+            for position in template.grouped_entries[chromosome]:
+                template_entries: set[RawEntry] = template.grouped_entries[chromosome][
+                    position
+                ]
                 query_entry = None
-                if chromosome not in query_entries:
-                    query_entry = self._null_result(target)
-                elif position not in query_entries[chromosome]:
-                    query_entry = self._null_result(target)
+                if chromosome not in query_entries.grouped_entries:
+                    query_entry = template.meta.undetermined
+                elif position not in query_entries.grouped_entries[chromosome]:
+                    query_entry = template.meta.undetermined
                 else:
-                    if len(query_entries[chromosome][position]) == 0:
+                    if len(query_entries.grouped_entries[chromosome][position]) == 0:
                         raise RuntimeError(
                             f"Bug: could not find a query entry for {chromosome} {position}"
                         )
-                    query_entry = query_entries[chromosome][position][0].result
-                    if len(query_entries[chromosome][position]) != 1:
+                    query_entry = query_entries.grouped_entries[chromosome][position][
+                        0
+                    ].result
+                    if len(query_entries.grouped_entries[chromosome][position]) != 1:
                         # Sanity check: we've multiple entries for the same chromosome
                         # and the same position. The genotype should be the same.
                         # If it's not, warn the user and set a null value.
-                        for entry in query_entries[chromosome][position]:
+                        for entry in query_entries.grouped_entries[chromosome][
+                            position
+                        ]:
                             if entry.result != query_entry:
-                                genomes = set(query_entries[chromosome][position])
+                                genomes = set(
+                                    query_entries.grouped_entries[chromosome][position]
+                                )
                                 logging.warn(
                                     f"Found different genotypes for the same "
                                     f"position and the same chromosome: {genomes!s}."
                                 )
                                 query_entry = self._null_result(target)
                                 break
-                ids = set()
                 for template_entry in template_entries:
-                    if template_entry.id in ids:
-                        # We've an entry with same chromosome/position.
-                        # If we need to print it more than once it must at least
-                        # have a different ID.
-                        continue
-                    ids.add(template_entry.id)
-                    output_line = TARGET_FORMATTER_MAP[target][0](
-                        template_entry.id,
-                        template_entry.chromosome,
-                        template_entry.position,
-                        query_entry,
+                    output_line = template.meta.output_format.format(
+                        id=template_entry.id,
+                        chromosome=template_entry.chromosome,
+                        position=template_entry.position,
+                        result=query_entry,
                     )
                     lines.append(output_line)
 
-        suffix = TARGET_FORMATTER_MAP[target][1].value
+        suffix = template.meta.file_extension
         stem = input.stem.replace("_CombinedKit", "")
         output = input.with_name(f"{stem}_{target.name}{suffix}")
 
         with output.open("a", newline="\n") as f:
-            for line in lines:
-                f.write(line)
+            f.writelines(template.comments)
+            f.writelines(lines)
+        return output
--- helix/microarray/raw_file.py
+++ helix/microarray/raw_file.py
@@ -1,17 +1,18 @@
 import gzip
-import json
 import logging
 from pathlib import Path
+from typing import Optional
 
 from parse import parse
 from pydantic import BaseModel
 
+from helix.naming.converter import Converter
 
-class RawEntry(BaseModel):
+
+class RawEntry(BaseModel, frozen=True):
     id: str
     chromosome: str
     position: int
-    format: str
     result: str = None
 
     @property
@@ -25,39 +26,80 @@ class RawEntry(BaseModel):
         return self.__str__()
 
 
+class MicroarrayMeta(BaseModel):
+    input_format: str = "{id}\t{chromosome}\t{position}\n"
+    output_format: str = "{id}\t{chromosome}\t{position}\t{result}\n"
+    file_extension: Optional[str] = None
+    undetermined: str = "00"
+
+
+class ParsedMicroarrayFile(BaseModel):
+    grouped_entries: dict[str, dict[int, set[RawEntry]]]
+    comments: Optional[list[str]] = None
+    meta: Optional[MicroarrayMeta] = None
+
+
 class RawFile:
     def __init__(self, path: Path) -> None:
         self.path = path
-        self.meta = None
+        self.meta: MicroarrayMeta = MicroarrayMeta()
         self._logger = logging.getLogger(__name__)
         if self.path.suffix == ".gz":
             self.open_file = lambda: gzip.open(self.path, "rt")
         else:
             self.open_file = lambda: self.path.open("rt")
 
-    def load_meta(self, line: str):
-        if self.meta is not None:
-            self._logger.warning(f"Found two metadata line in {self.path!s}")
-            return
-        line = line.removeprefix("##")
-        self.meta = json.loads(line)
+    def parse(self) -> ParsedMicroarrayFile:
+        grouped: dict[str, dict[int, set[RawEntry]]] = dict()
+        meta: MicroarrayMeta = MicroarrayMeta()
+        comments = []
 
-    def __iter__(self):
         with self.open_file() as file:
             for line in file:
                 if line.startswith("##"):
-                    self.load_meta(line)
+                    line = line.removeprefix("##")
+                    if meta is not None:
+                        self._logger.warning(
+                            f"Duplicate metadata line found in {self.path!s}"
+                        )
+                    meta = MicroarrayMeta.model_validate_json(line)
                     continue
-                if "#" in line:
-                    yield line
+                elif line.startswith("#"):
+                    comments.append(line)
                     continue
-                parsed = parse(self.meta["format"], line)
+                parsed = parse(self.meta.output_format, line)
                 if parsed is None:
-                    yield None
-                    continue
-                yield RawEntry(
+                    parsed = parse(self.meta.input_format, line)
+                    if parsed is None:
+                        self._logger.debug(
+                            f"Found invalid line in {self.path!s}: {self.meta.input_format}!={line}"
+                        )
+                        continue
+
+                entry = RawEntry(
                     id=parsed["id"],
                     chromosome=parsed["chromosome"],
                     position=parsed["position"],
-                    format=self.meta["format"],
+                    result=parsed["result"] if "result" in parsed else None,
                 )
+                if isinstance(entry, str):
+                    comments.append(entry)
+                    continue
+                chromosome = Converter.canonicalize(entry.chromosome)
+                if chromosome not in grouped:
+                    grouped[chromosome] = dict()
+                if entry.position not in grouped[chromosome]:
+                    grouped[chromosome][entry.position] = set()
+                grouped[chromosome][entry.position].add(entry)
+
+        if meta.file_extension is None:
+            if len(self.path.suffixes) > 1 and self.path.suffixes[-1] == ".gz":
+                meta.file_extension = self.path.suffixes[-2]
+            elif len(self.path.suffixes) > 0:
+                meta.file_extension = self.path.suffixes[-1]
+
+        return ParsedMicroarrayFile(
+            grouped_entries=grouped,
+            comments=comments,
+            meta=meta,
+        )
