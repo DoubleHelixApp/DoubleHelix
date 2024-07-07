4ef235d1885a1cc5656a313bbc185142b49d1620
chaplin89 <2357260+chaplin89@users.noreply.github.com>
2024-07-07 09:58:19 +0200

    Removed main() from main.py

--- helix/main.py
+++ helix/main.py
@@ -6,8 +6,7 @@ from helix.utility.check_prerequisites import CheckPrerequisites
 
 SENTRY = "https://7942f481884f55dfc85350ef336b3299@o4507282907856896.ingest.de.sentry.io/4507282933874768"  # noqa: E501
 
-
-def main():
+if __name__ == "__main__":
     sentry_sdk.init(
         dsn=SENTRY,
         traces_sample_rate=1.0,
@@ -15,7 +14,3 @@ def main():
     )
     CheckPrerequisites().check_prerequisites()
     helix.gui.main()
-
-
-if __name__ == "__main__":
-    main()
