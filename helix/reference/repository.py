aebf7893cb2cb4f081d6ec3112c4b564fccb03ea
chaplin89 <chaplin89>
2026-01-26 18:46:40 +0100

    Added comments;  cosmetic changes

--- helix/reference/repository.py
+++ helix/reference/repository.py
@@ -1,6 +1,6 @@
 import logging
 from collections import OrderedDict
-from typing import Callable
+from typing import Callable, Optional
 
 from helix.alignment_map.alignment_map_header import AlignmentMapHeader
 from helix.configuration import RepositoryConfig
@@ -26,41 +26,73 @@ class Repository:
 
     def __init__(
         self,
-        loader: MetadataLoader = MetadataLoader(),
-        downloader: Downloader = Downloader(),
-        mtdna: MtDNA = MtDNA(),
-        config: RepositoryConfig = RepositoryConfig(),
+        loader: Optional[MetadataLoader] = None,
+        downloader: Optional[Downloader] = None,
+        mtdna: Optional[MtDNA] = None,
+        config: Optional[RepositoryConfig] = None,
     ) -> None:
-        self._loader = loader
-        self._downloader = downloader
-        self._mtdna = mtdna
+        self._loader = loader or MetadataLoader()
+        self._downloader = downloader or Downloader()
+        self._mtdna = mtdna or MtDNA()
+        self._config = config or RepositoryConfig()
+        
         self.genomes = self._loader.load()
-        # Index individual sequences by their MD5 and length
+
+        # Map MD5s to sequences
         self._sequences_by_md5: dict[str, list[Sequence]] = self._group_sequences(
             lambda s: s.md5, self.genomes
         )
+        # Map lengths to sequences
         self._sequences_by_length: dict[int, list[Sequence]] = self._group_sequences(
             lambda s: s.length, self.genomes
         )
-        self._config = config
 
     def analyze_references(self) -> None:
-        # Analyzing sequence by sequence
+        """
+        This is a debug function made to analyze ambiguous cases:
+        - genomes with sequences having the same length but different MD5s
+        - genomes with same sequence of lengths having different MD5s (worst case)
+
+        Example:
+            - Genome 1, has sequences:
+                - A, length: 10, MD5: X
+                - B, length: 11, MD5: Y
+            - Genome 2, has sequences:
+                - A, length: 10, MD5: Z
+                - B, length: 12, MD5: T
+            - Genome 3, has sequences:
+                - A, length: 10, MD5: W
+                - B, length: 11, MD5: H
+        
+        The first pass will highlight Genome 1, sequence A and Genome 2 sequence A.
+        It's weird if A and B are chromosome's sequences, but is still not super problematic
+        because the full sequence of length (10,11) is a fingerprint for the genome.
+
+        The second pass will highlight Genome 1, Genome 3. This is the worst case as you cannot
+        use the sequence of length (10,11) to fingerprint the genome.
+        
+        :param self: Description
+        """
+
+        # Genomes with sequences having the same length but different MD5s
         different_md5_same_length = []
-        for length, seqs in self._sequences_by_length.items():
-            unique_seq_set = set()
+        for length, sequences in self._sequences_by_length.items():
+            unique_sequences = set()
             name = None
-            for seq in seqs:
-                unique_seq_set.add(seq.md5)
-                name = seq.name
-            if len(unique_seq_set) > 1 and length > 57227414:
+            for sequence in sequences:
+                unique_sequences.add(sequence.md5)
+                name = sequence.name
+            # There will be a lot of matches for small sequences
+            # but the most interesting one are chromosomes. 
+            # This number is just an heuristic to try to filter 
+            # out small sequences.
+            if len(unique_sequences) > 1 and length > 57227414:
                 different_md5_same_length.append(length)
                 logging.debug(
-                    f"There are {len(unique_seq_set)} different MD5 for length {length} ({name})."
+                    f"There are {len(unique_sequences)} different MD5s for length {length} ({name})."
                 )
 
-        # Analyzing the whole genome, looking for identical length sequences
-        # with different MD5 sequences
+        # Genomes with same sequence of lengths having different MD5s (worst case)
         different_md5s_same_lengths = {}
         for genome in self.genomes:
             if genome.sequences is None:
@@ -141,6 +173,19 @@ class Repository:
     def _create_companion_files(
         self, genome: Genome, force=False, progress=None, action: str = None
     ):
+        """
+        Given a genome, create companion files:
+        - bgzip index
+        - dictionary
+        
+        :param self: Description
+        :param genome: Description
+        :type genome: Genome
+        :param force: Description
+        :param progress: Description
+        :param action: Description
+        :type action: str
+        """
         samtools = Samtools()
         bgzip = BGzip()
         logging.info(f"{genome}: Starting post-download tasks.")
@@ -157,6 +202,13 @@ class Repository:
             logging.info(f"{genome}: Dictionary file exists.")
 
     def self_test(self):
+        """
+        Perform a sanity check on the genome database.
+        If a genome is registered but has no sequences, 
+        perform the steps to obtain them.
+        
+        :param self: Description
+        """
         for index, reference in enumerate(self.genomes):
             if reference.sequences is None:
                 try:
