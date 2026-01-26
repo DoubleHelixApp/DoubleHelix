a46cbcefa2de2531d8abd2e96ee52d03408cf036
chaplin89 <chaplin89>
2026-01-26 18:40:28 +0100

    Fixed `helix` commandline

--- helix/main.py
+++ helix/main.py
@@ -6,7 +6,10 @@ from helix.utility.check_prerequisites import CheckPrerequisites
 
 SENTRY = "https://7942f481884f55dfc85350ef336b3299@o4507282907856896.ingest.de.sentry.io/4507282933874768"  # noqa: E501
 
-if __name__ == "__main__":
+def main():
+    """
+    I'm the entry point for the helix executable.
+    """
     sentry_sdk.init(
         dsn=SENTRY,
         traces_sample_rate=1.0,
@@ -14,3 +17,7 @@ if __name__ == "__main__":
     )
     CheckPrerequisites().check_prerequisites()
     helix.gui.main()
+
+
+if __name__ == "__main__":
+    main()
